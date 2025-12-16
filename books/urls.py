d0fb6508872799f688cb8b8180edf72aa059eb89
Orifjon Kenjayev <128310421+OrifjonKenjayev@users.noreply.github.com>
2025-12-16 19:14:44 +0500

    new features

--- books/urls.py
+++ books/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 
 app_name = 'books'
@@ -13,5 +14,8 @@ path('categories/<slug:category_slug>/<slug:subcategory_slug>/', views.book_list
 path('book/<int:pk>/', views.book_detail, name='book_detail'),
 path('book/<int:pk>/download/', views.book_download, name='book_download'),
 path('upload/', views.upload_book, name='upload_book'),
- path('search/', views.search_results, name='search_results'),
+path('search/', views.search_results, name='search_results'),
+path('contact/', TemplateView.as_view(template_name='books/contact.html'), name='contact'),
+
+
 ]
